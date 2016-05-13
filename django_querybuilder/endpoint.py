a719b8f1e1bc9139c9755c80b617ab3ca7421abc
Michał Łuszczyk <m0@qed.ai>
2016-05-13 16:45:01 +0200

    Refactor in endpoint.
    Change-Id: I8951f526540273e83e24ce48a7b55a21c27a592a

--- django_querybuilder/endpoint.py
+++ django_querybuilder/endpoint.py
@@ -30,19 +30,20 @@ class QuerybuilderEndpoint(View):
         widget_params = request.POST.get('widget_params')
         if widget_params:
             widget_params = json.loads(widget_params)
-        widget = self.get_widget_by_id(request.POST.get('widget_id'), widget_params)
-        if widget:
+        meta_widget = self.get_meta_widget_by_id(request.POST.get('widget_id'))
+        if meta_widget is not None:
+            widget = meta_widget.fill(widget_params)
             data = widget.get_data(request.POST.get('query_config'))
             return JsonResponse({'status': 'OK', 'data': data})
 
         return JsonResponse({'status': 'WIDGET_NOT_FOUND'})
 
-    def get_widget_by_id(self, widget_id, params):
+    def get_meta_widget_by_id(self, widget_id):
         for attribute_name in dir(self.querybuilder):
             Item = getattr(self.querybuilder, attribute_name)
             try:
                 if isinstance(Item, MetaWidget) and Item.name == widget_id:
-                    return Item.fill(params)
+                    return Item
             except TypeError:
                 continue
         return None
