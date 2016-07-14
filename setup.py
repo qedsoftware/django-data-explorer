dfbf4171b68120aa557265728336897f5de17e35
Michał Łuszczyk <m0@qed.ai>
2016-07-14 09:43:21 +0300

    Release 0.3.0
    Change-Id: I5ff7725eb7d70eabb3430ab7d28d2f28b1551edb

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-version = '0.0.2'
+version = '0.0.3'
 
 setup(
     name='django-querybuilder',
