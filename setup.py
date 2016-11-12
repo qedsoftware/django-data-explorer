94eb7c9004412e2e8fa91ce2c98ba1dc5cf17a99
Tomasz Kępa <tomek@qed.ai>
2016-11-12 16:58:23 +0100

    Bump version to 0.0.6
    Change-Id: Ibac94c0a4499ca706ae221f7fe90f5d3b377ed5f

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-version = '0.0.5'
+version = '0.0.6'
 
 setup(
     name='django-data-explorer',
